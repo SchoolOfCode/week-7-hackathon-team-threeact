7df2926bd4edd40ee53694fc5a01194a77d6f037
Jonathan Lee <kutsu89@gmail.com>
2025-01-23 13:29:19 +0000

    form component created

--- my-vite-app/src/App.jsx
+++ my-vite-app/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header";
+import FlashForm from "./components/form/Form.jsx";
 
 function App() {
   return (
     <>
       <Header />
+      <FlashForm />
     </>
   );
 }
